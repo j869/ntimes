7bb6861f25ba8a97174b0fc1dce0426f2c7a7d70
unknown <edflores240@gmail.com>
2024-04-02 01:26:06 +0800

    first commit

--- server/index.js
+++ server/index.js
@@ -1,40 +1,36 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import * as db from './queries.js';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import * as db from "./queries.js";
 //#region middleware
-const port = 4000
+const port = 4000;
 const app = express();
-app.use(cors())             //Handling CORS issues
-app.use(bodyParser.json())
+app.use(cors()); //Handling CORS issues
+app.use(bodyParser.json());
 app.use(
-bodyParser.urlencoded({
+  bodyParser.urlencoded({
     extended: true,
-})
-)
+  })
+);
 //#endregion
 
+app.get("/users", db.getUsers);
+app.get("/login/:username", db.getUserByUsername);
+app.get("/users/:id", db.getUserById);
+app.post("/users", db.createUser);
+app.put("/users/:id", db.updateUser);
+app.put("/verify/:token", db.verifyUserEmail);
+app.delete("/users/:id", db.deleteUser);
 
+app.get("/rdo/:id", db.getRdoById);
 
-app.get('/users', db.getUsers)
-app.get('/login/:username', db.getUserByUsername)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.put('/verify/:token', db.verifyUserEmail)
-app.delete('/users/:id', db.deleteUser)
+app.get("/location/:id", db.getLocation);
 
-app.get('/rdo/:id', db.getRdoById)
-
-app.get('/timesheets/:id', db.getCurrentYearTimesheetsForUser)
-app.put('/timesheets', db.createTimesheet)
-app.post('/timesheets/:id/updateStatus', db.updateTimesheetStatus)
-app.delete('/timesheets/:id', db.deleteTimesheet)
-
-
-
-
+app.get("/timesheets/:id", db.getCurrentYearTimesheetsForUser);
+app.put("/timesheets", db.createTimesheet);
+app.post("/timesheets/:id/updateStatus", db.updateTimesheetStatus);
+app.delete("/timesheets/:id", db.deleteTimesheet);
 
 app.listen(port, () => {
-console.log(`App running on port ${port}.`)
-})
+  console.log(`App running on port ${port}.`);
+});
